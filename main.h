cda072902bf746e74453bf63fb6096751a450880
Tobias Schramm <tobleminer@gmail.com>
2018-01-17 16:27:33 +0100

    Remove obsolete main struct

--- main.h
+++ main.h
@@ -3,23 +3,4 @@
 
 #include "network.h"
 
-enum {
-	TX_METHOD_SENDFILE,
-	TX_METHOD_SEND
-};
-
-struct threadargs_t {
-	int tid;
-	int socket;
-	int* sockets;
-	struct sockaddr* remoteaddr;
-	int method;
-
-	FILE* file;
-	char* buffer;
-
-	off_t offset;
-	size_t length;
-};
-
 #endif
